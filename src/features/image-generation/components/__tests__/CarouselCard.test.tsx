8e545f4b6421864f4a24f4dbc35ab2c1db226ac6
Jhon Everton <jhonevertonapp@outlook.com>
2025-12-23 20:11:08 -0300

    feat: add unit tests for CarouselCard component.

--- src/features/image-generation/components/__tests__/CarouselCard.test.tsx
+++ src/features/image-generation/components/__tests__/CarouselCard.test.tsx
@@ -6,7 +6,8 @@ import { CarouselCard as CarouselCardType } from "../../types";
 // Mock next/image
 jest.mock("next/image", () => ({
   __esModule: true,
-  default: (props: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    default: (props: any) => {
     // eslint-disable-next-line @next/next/no-img-element
     return <img {...props} alt={props.alt} />;
   },
